e8eb29943fb1d4ed3049927e986ddc017cd9d494
Raunak88 <raunak.kumar88@gmail.com>
2014-09-28 15:15:37 +0530

    Feat(Checked in EmployeeApp)

---
+++ app/js/controllers.js
@@ -0,0 +1,64 @@
+app.controller("EmployeeDataCtrl", function ($http, $scope) {
+    $scope.index = "";
+    if (localStorage.getItem('localStorageKey') == null)
+    {
+        $http.get('img/Employee.json').success(function (data) {
+            var info = angular.toJson(data);
+            localStorage.setItem('localStorageKey', info);
+            $scope.Employee = localStorage.getItem('localStorageKey');
+            $scope.Employee = angular.fromJson($scope.Employee);
+
+        });
+    }
+    else
+    {
+        $scope.Employee = localStorage.getItem('localStorageKey');
+        $scope.Employee = angular.fromJson($scope.Employee);
+    }
+    $scope.EmployeeDelete = function (index) {
+
+
+        $scope.Employee.splice(index, (index + 1));
+        localStorage.setItem('localStorageKey', JSON.stringify($scope.Employee));
+    };
+    $scope.EmployeeEditfunc = function (index) {
+        
+        $scope.editData = true;
+
+        $scope.EmployeeEdit = $scope.Employee[index];
+        $scope.index = index;
+    }
+    $scope.saveData = function ()
+    {
+        if ($scope.index !== "" && angular.isDefined($scope.index)) {
+            $scope.Employee[$scope.index].ID = $scope.EmployeeEdit.ID;
+            $scope.Employee[$scope.index].Name = $scope.EmployeeEdit.Name;
+            $scope.Employee[$scope.index].Location = $scope.EmployeeEdit.Location;
+            $scope.Employee[$scope.index].DOB = $scope.EmployeeEdit.DOB;
+            $scope.editData = false;
+            localStorage.setItem('localStorageKey', JSON.stringify($scope.Employee));
+        }
+        else {
+            console.log($scope.EmployeeEdit);
+            $scope.Employee.push(
+                    {ID: $scope.EmployeeEdit.ID,
+                        Name: $scope.EmployeeEdit.Name,
+                        Location: $scope.EmployeeEdit.Location,
+                        DOB: $scope.EmployeeEdit.DOB}
+            );
+
+            $scope.editData = false;
+            localStorage.setItem('localStorageKey', JSON.stringify($scope.Employee));
+        }
+
+        $scope.index = "";
+
+    }
+    $scope.cancelData = function()
+    {
+        $scope.editData = false;
+    }
+    
+});
+
+
